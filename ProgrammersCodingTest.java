361b5b69e9609f776e3dc4bd57368509b2bc8c44
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-04-06 20:20:15 +0900

    LV1 푸드 파이트 대회

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,69 +1,46 @@
 package codingTest;
 
-import java.util.Arrays;
-
 public class ProgrammersCodingTest {
 
 	public static void main(String[] args) {
-		String str = "1111111";
+		int[] arrFood = new int[] {1, 7, 1, 2};
 		
-		solution(str);
+		solution(arrFood);
 	}
 
-	// 이진 변환 반복하기
-//	static int lastDelCnt = 0;
-//	static int chgCnt 	  = 0;	// 이진 변환 회차
-	static int[] answer = new int[2];
-	public static int[] solution(String s) {
-		System.out.println("answer : " + Arrays.toString(answer));
-		int lastDelCnt = 0;
-		int chgCnt 	   = 0;	// 이진 변환 회차
-		
-		if (!"1".equals(s)) {
-			removeProc(s, lastDelCnt, chgCnt);
-		}
-
-		return answer;
-    }
-
-	public static void removeProc(String s, int lastDelCnt, int chgCnt) {
-		String[] arrStr = s.split("");
+	// 푸드 파이트 대회
+	public static String solution(int[] food) {
+        String answer = "";
         
-        String newStr = "";
-        int delCnt = 0;		// 	제거할 0의 개수
-        int newLen = 0;
-        for (int i = 0; i < arrStr.length; i++) {
-//        	System.out.println("arrStr[i] : " + arrStr[i]);
-        	
-        	if (!"0".equals(arrStr[i])) {
-        		newStr += arrStr[i];
-        	} else {
-        		delCnt += 1;
+        for (int i = 0; i < food.length; i++) {
+        	// 물이 아닐때
+        	if (i != 0) {
+        		for (int j = 0; j < food[i] / 2; j++) {
+        			answer += i;
+        		}
+        		
+//        		if (food[i] % 2 == 0) {
+//        			for (int j = 0; j < food[i] / 2; j++) {
+//        				System.out.println(j);
+//        				answer += i;
+//        			}
+//        			System.out.println();    				
+//        		} else {
+//        			
+//        		}
         	}
         }
         
-        chgCnt += 1;
-        newLen += newStr.length();
+        answer += "0";
         
-        System.out.println("delCnt : " + delCnt);
-        System.out.println("newLen : " + newLen);
-        System.out.println("newStr : " + newStr);
-        System.out.println("이진 변환 결과 : " + Integer.toBinaryString(newLen));
-        
-        lastDelCnt += delCnt;
-        
-        System.out.println("lastDelCnt : " + lastDelCnt);
-        System.out.println("chgCnt : " + chgCnt);
-        System.out.println();
-        
-        if (!"1".equals(Integer.toBinaryString(newLen))) {
-        	removeProc(Integer.toBinaryString(newLen), lastDelCnt, chgCnt);
-        } else {
-        	answer[0] = chgCnt;
-        	answer[1] = lastDelCnt;
-        	
-        	solution(Integer.toBinaryString(newLen));
+        for (int i = food.length - 1; i >= 0; i--) {
+        	for (int j = 0; j < food[i] / 2; j++) {
+    			answer += i;
+    		}
         }
-	}
+        
+        System.out.println("answer : " + answer);
+        return answer;
+    }
 	
 }
