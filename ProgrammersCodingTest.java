70c40073a0b3fe9ffa6f3d45534e5312c0011be5
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-04-11 21:20:52 +0900

    LV2 다음 큰 숫자

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,46 +1,40 @@
 package codingTest;
 
-import java.util.Arrays;
-
 public class ProgrammersCodingTest {
 
 	public static void main(String[] args) {
-		String str = "foobar";
+		int num = 78;
 		
-		solution(str);
+		solution(num);
 	}
 
-	// 가장 가까운 같은 글자
-	public static int[] solution(String s) {
-        int[] answer = new int[s.length()];
-        String[] arrStr = s.split("");
+	// 다음 큰 숫자
+	public static int solution(int n) {
+        int answer = 0;
+        // long numCnt = Integer.toBinaryString(n).chars().filter(c -> c == '1').count();	// *시간초과
+        int numCnt = Integer.toBinaryString(n).length() - Integer.toBinaryString(n).replace("1", "").length();
+        	System.out.println("numCnt : " + numCnt);
         
-        for (int i = 0; i < arrStr.length; i++) {
-        	System.out.println(i);
-        	System.out.println(arrStr[i]);
+        while (true) {
+//        	System.out.println("현재 n : " + n);
+//        	System.out.println("현재 n : " + Integer.toBinaryString(n));
+        	n++;
         	
-        	int idx = s.indexOf(arrStr[i]);
-        	int cnt = 0;
-        	while (idx != -1) {
-        		System.out.println("idx : " + idx);
-        		System.out.println("cnt : " + cnt);
-        		cnt = idx;
-        		idx = s.indexOf(arrStr[i], idx + 1);
-
-        		if (idx == i) {
-        			System.out.println("들어옴");
-        			answer[i] = idx - cnt;
-        			break;
-        		} else {
-        			answer[i] = -1;
-        		}
-        		
-//        		cnt++;
-        	}
+        	// long nextCnt = Integer.toBinaryString(n).chars().filter(c -> c == '1').count();
+        	int nextCnt = Integer.toBinaryString(n).length() - Integer.toBinaryString(n).replace("1", "").length();
+        	
+        	System.out.println("다음 n : " + n);
+        	System.out.println("다음 n : " + Integer.toBinaryString(n));
+        	System.out.println("nextCnt : " + nextCnt);
         	System.out.println();
+        	
+        	if (numCnt == nextCnt) {
+        		answer = n;
+        		break;
+        	}
         }
         
-        System.out.println("answer : " + Arrays.toString(answer));
+        System.out.println("최종 n : " + n);
         return answer;
     }
 }
