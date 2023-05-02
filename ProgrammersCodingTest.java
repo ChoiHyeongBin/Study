a4226801a50d6ffd34dbfd062ff7253f748c56ef
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-05-02 20:45:31 +0900

    LV2 영어 끝말잇기

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,55 +1,63 @@
 package codingTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.List;
 
 public class ProgrammersCodingTest {
 
 	public static void main(String[] args) {
-		String[] arrPlayers = new String[] {"mumu", "soe", "poe", "kai", "mine"};
-		String[] arrCallings = new String[] {"kai", "kai", "mine", "mine"};
+		int num = 2;
+		String[] arrStr = new String[] {"hello", "one", "even", "never", "now", "world", "draw"};
 		
-		solution(arrPlayers, arrCallings);
+		solution(num, arrStr);
 	}
 
-	// LV1 달리기 경주
-	public static String[] solution(String[] players, String[] callings) {
-        String[] answer = new String[players.length];
-        
-        HashMap<String, Integer> mappedByPlayer = new HashMap<>();
-        HashMap<Integer, String> mappedByRank = new HashMap<>();
-        
-        for (int i = 0; i < players.length; i++) {
-        	mappedByPlayer.put(players[i], i);
-        	mappedByRank.put(i, players[i]);
-        }
-        System.out.println("mappedByPlayer : " + mappedByPlayer);
-        System.out.println("mappedByRank : " + mappedByRank);
-        System.out.println();
-        
-        for (int i = 0; i < callings.length; i++) {
-        	int currentRank = mappedByPlayer.get(callings[i]);	// 추월 유저 순위
-        	String player = mappedByRank.get(currentRank);
-        	System.out.println("currentRank : " + currentRank);
-        	System.out.println("player : " + player);
+	// LV2 영어 끝말잇기
+	public static int[] solution(int n, String[] words) {
+		int[] answer = new int[2];
+		List<String> list = new ArrayList<String>();
+		boolean flag = true;
+		
+		for (int i = 0; i < words.length; i++) {
+			System.out.println("words: " + words[i]);
+			
+			if (i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)
+				|| list.contains(words[i])) {	// 이전에 등장한 단어
+				answer[0] = (i % n) + 1;	// 사람 번호
+				answer[1] = (i / n) + 1;	// 차례
+				flag = false;
+				break;
+			}
+			
+			list.add(words[i]);
+		}
+		
+		if (flag)	answer = new int[] {0, 0};
+		System.out.println("answer: " + Arrays.toString(answer));
+		return answer;
+		
+        /* int[] answer = {};
+
+        int cnt = 0;
+        for (int i = 0; i < words.length; i++) {
+        	if (i + 1 == words.length) {
+        		i = 0;
+        	}
         	
-        	String frontPlayer = mappedByRank.get(currentRank - 1);
+        	System.out.println("words 마지막글자: " + words[i].substring(words[i].length() - 1));
+        	System.out.println("words 첫번째글자: " + words[i + 1].substring(0, 1));
+        	System.out.println("words : " + words[i]);
         	
-        	mappedByPlayer.put(player, currentRank - 1);
-        	mappedByPlayer.put(frontPlayer, currentRank);
+        	if (!words[i].substring(words[i].length() - 1).equals(words[i + 1].substring(0, 1))) {
+        		break;
+        	}
         	
-        	mappedByRank.put(currentRank - 1, player);
-        	mappedByRank.put(currentRank, frontPlayer);
+        	if () {
+        		
+        	}
         }
-        System.out.println("mappedByPlayer 222 : " + mappedByPlayer);
-        System.out.println("mappedByRank 222 : " + mappedByRank);
-        
-        for (int i = 0; i < players.length; i++) {
-        	answer[i] = mappedByRank.get(i);
-        }
-        
-        System.out.println(Arrays.toString(answer));
-        
-        return answer;
+
+        return answer; */
     }
 }
