479cec88856253afe6dded28ed3887824aa71bdb
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-05-04 15:51:14 +0900

    LV1 추억 점수

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,63 +1,47 @@
 package codingTest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProgrammersCodingTest {
 
 	public static void main(String[] args) {
-		int num = 2;
-		String[] arrStr = new String[] {"hello", "one", "even", "never", "now", "world", "draw"};
+		String[] name = new String[] {"may", "kein", "kain", "radi"};
+		int[] yearning = new int[] {5, 10, 1, 3};
+		String[][] photo = {{"may"},{"kein", "deny", "may"}, {"kon", "coni"}};
 		
-		solution(num, arrStr);
+		solution(name, yearning, photo);
 	}
 
-	// LV2 영어 끝말잇기
-	public static int[] solution(int n, String[] words) {
-		int[] answer = new int[2];
-		List<String> list = new ArrayList<String>();
-		boolean flag = true;
-		
-		for (int i = 0; i < words.length; i++) {
-			System.out.println("words: " + words[i]);
-			
-			if (i > 0 && words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)
-				|| list.contains(words[i])) {	// 이전에 등장한 단어
-				answer[0] = (i % n) + 1;	// 사람 번호
-				answer[1] = (i / n) + 1;	// 차례
-				flag = false;
-				break;
-			}
-			
-			list.add(words[i]);
-		}
-		
-		if (flag)	answer = new int[] {0, 0};
-		System.out.println("answer: " + Arrays.toString(answer));
-		return answer;
-		
-        /* int[] answer = {};
-
-        int cnt = 0;
-        for (int i = 0; i < words.length; i++) {
-        	if (i + 1 == words.length) {
-        		i = 0;
-        	}
-        	
-        	System.out.println("words 마지막글자: " + words[i].substring(words[i].length() - 1));
-        	System.out.println("words 첫번째글자: " + words[i + 1].substring(0, 1));
-        	System.out.println("words : " + words[i]);
-        	
-        	if (!words[i].substring(words[i].length() - 1).equals(words[i + 1].substring(0, 1))) {
-        		break;
-        	}
-        	
-        	if () {
-        		
-        	}
+	// LV1 추억 점수
+	public static int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> tempMap = new HashMap<String, Integer>();
+        System.out.println(Arrays.toString(answer));
+        System.out.println(photo.length);
+        
+        for (int i = 0; i < name.length; i++) {
+        	tempMap.put(name[i], yearning[i]);
         }
-
-        return answer; */
+        
+        for (int i = 0; i < photo.length; i++) {
+        	int sum = 0;
+        	
+        	for (int j = 0; j < photo[i].length; j++) {
+        		System.out.println("photo[i][j]: " + photo[i][j] + ", 존재 여부: " 
+        			+ tempMap.containsKey(photo[i][j]));
+        		
+        		if (tempMap.containsKey(photo[i][j])) {
+        			sum += tempMap.get(photo[i][j]);
+        		}
+        	}
+        	System.out.println("sum: " + sum);
+        	System.out.println();
+        	answer[i] = sum;
+        }
+        
+        System.out.println(Arrays.toString(answer));
+        return answer;
     }
 }
