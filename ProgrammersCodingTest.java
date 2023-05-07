7ec916e430d7cb25765c4b2a93642c1f9732a8f1
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-05-07 22:58:10 +0900

    LV2 카펫

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,44 +1,37 @@
 package codingTest;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProgrammersCodingTest {
 
 	public static void main(String[] args) {
-		String[] name = new String[] {"may", "kein", "kain", "radi"};
-		int[] yearning = new int[] {5, 10, 1, 3};
-		String[][] photo = {{"may"},{"kein", "deny", "may"}, {"kon", "coni"}};
+		int brown = 10;
+		int yellow = 2;
 		
-		solution(name, yearning, photo);
+		solution(brown, yellow);
 	}
 
-	// LV1 추억 점수
-	public static int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
-        Map<String, Integer> tempMap = new HashMap<String, Integer>();
-        System.out.println(Arrays.toString(answer));
-        System.out.println(photo.length);
+	// LV2 카펫
+	public static int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;	// 격자 총갯수
         
-        for (int i = 0; i < name.length; i++) {
-        	tempMap.put(name[i], yearning[i]);
-        }
-        
-        for (int i = 0; i < photo.length; i++) {
-        	int sum = 0;
+        for (int i = 3; i < sum; i++) {
+        	int j = sum / i;
+//        	System.out.println("j : " + j);
         	
-        	for (int j = 0; j < photo[i].length; j++) {
-        		System.out.println("photo[i][j]: " + photo[i][j] + ", 존재 여부: " 
-        			+ tempMap.containsKey(photo[i][j]));
+        	if (sum % i == 0 && j >= 3) {
+        		int col = Math.max(i, j);
+        		int row = Math.min(i, j);
+        		System.out.println("col: " + col + ", row: " + row);
+        		int center = (col - 2) * (row - 2);
+        		System.out.println("center: " + center);
         		
-        		if (tempMap.containsKey(photo[i][j])) {
-        			sum += tempMap.get(photo[i][j]);
+        		if (center == yellow) {
+        			answer[0] = col;
+        			answer[1] = row;
         		}
         	}
-        	System.out.println("sum: " + sum);
-        	System.out.println();
-        	answer[i] = sum;
         }
         
         System.out.println(Arrays.toString(answer));
