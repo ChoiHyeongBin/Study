1925932e95ce0348ee0fe6a79db4290b2e6f3219
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-06-04 19:20:35 +0900

    LV1 과일 장수

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,30 +1,49 @@
 package codingTest;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
 public class ProgrammersCodingTest {
 
     public static void main(String[] args) {
-    	int n = 5000;
+    	int k = 3;
+    	int m = 4;
+//    	int[] score = new int[] {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
+    	int[] score = new int[] {1, 2, 3, 1, 2, 3, 1};
     	
-    	solution(n);
+    	solution(k, m, score);
     }
 	
-    // LV2 점프와 순간 이동
-    public static int solution(int n) {
-        int ans = 0;
+    // LV1 과일 장수
+    public static int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Integer[] tmp = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(tmp, Comparator.reverseOrder());
+        
+        System.out.println(Arrays.toString(tmp));
 
-        while (n != 0) {
-        		System.out.println(n);
-        	if (n % 2 == 0) {	// 2
-        		n /= 2;
-        	} else {	// 2
-        		n -= 1;
-        		// n--;
-        		ans++;
+        // *시간 초과
+//        LinkedList<Integer> newArr = new LinkedList<Integer>();
+        
+//        for (Integer i : tmp) {
+//        	newArr.add(i);
+//        }
+        
+//        System.out.println(newArr);
+        
+        int cnt = 0;
+        for (int i = 0; i < tmp.length; i++) {
+        	cnt++;
+        	
+        	if (cnt == m) {
+        		answer += tmp[i] * m;
+        		cnt = 0;
         	}
         }
-
-        System.out.println("ans: " + ans);
-        return ans;
+        
+        System.out.println("answer: " + answer);
+        return answer;
     }
 
 }
