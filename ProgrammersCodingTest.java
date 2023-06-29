8725b83ee0a7f2b30432488b59261366c2966a57
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-06-29 15:40:11 +0900

    LV2 행렬의 곱셈

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,47 +1,69 @@
 package codingTest;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class ProgrammersCodingTest {
 
     public static void main(String[] args) {
-    	int[] elements = new int[] {7,9,1,1,4};
+//    	int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+    	int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+//    	int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+    	int[][] arr2 = {{3, 3}, {3, 3}};
     	
-    	solution(elements);
+    	solution(arr1, arr2);
     }
 	
-    // LV2 연속 부분 수열 합의 개수
-    public static int solution(int[] elements) {
-        int answer = 0;
-        // 1.7 부터는 앞에 선언된 타입으로 컴파일러에서 추측이 가능 하기 때문에 뒤에 생성자에서 제네릭 생략 가능하다. 
-        // 이걸 다이아몬드 표현이라고 한다.
-        Set<Integer> tempSet = new HashSet<Integer>();
+    // LV2 행렬의 곱셈
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+//        System.out.println(Arrays.deepToString(answer));
         
-        // 연속적인 수
-        for (int i = 1; i <= elements.length; i++) {
-        	// 배열 자릿수
-        	for (int j = 0; j < elements.length; j++) {
-        		int sum = 0;
+        for (int i = 0; i < arr1.length; ++i) {
+        	System.out.println(Arrays.toString(arr1[i]));
+        	
+        	for (int j = 0; j < arr2[0].length; ++j) {
+        		for (int k = 0; k < arr1[0].length; ++k) {
+        			System.out.println(arr1[i][k]);
+        			System.out.println(arr2[k][j]);
+        			System.out.println();
+        			
+        			answer[i][j] += arr1[i][k] * arr2[k][j];
+        		}
+        	}
+        }
+        
+        /* 실패
+        int cnt = 0;	// 배열 전체 개수
+        
+        for (int i = 0; i < arr1.length; i++) {
+        	System.out.println(Arrays.toString(arr1[i]));
 
-        		for (int k = 0; k < i; k++) {
-        			if (j + k > elements.length - 1) {
-        				sum += elements[j + k - elements.length];
-        			} else {
-        				sum += elements[j + k];
-        			}
-                }
+        	cnt += arr1[i].length;
+        }
+        
+        System.out.println("cnt: " + cnt);
+        
+        for (int i = 0; i < arr1.length; i++) {
+        	System.out.println(Arrays.toString(arr1[i]));
+        	
+        	for (int j = 0; j < cnt; j++) {
+        		int n = 0;
         		
-        		tempSet.add(sum);
-        		System.out.println("sum: " + sum);
-            }
+        		for (int k = 0; k < arr1[i].length; k++) {
+            		System.out.println(arr1[i][n] * arr2[k][i]);
+            		n++;
+            		
+            		if (n >= arr1[i].length) {
+            			n = 0;
+            		}
+                }
+        	}
         	
         	System.out.println();
         }
+         */
         
-        System.out.println("tempSet: " + tempSet.size());
-        
-        answer = tempSet.size();
+        System.out.println(Arrays.deepToString(answer));
         return answer;
     }
 	
