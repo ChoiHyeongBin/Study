5aacdf47c63e0099832c95b120dcb72300a8a2e7
ChoiHyeongBin <chekqhksha@gmail.com>
2023-07-06 22:33:53 +0900

    Java 설정 스프링 프로젝트 생성

---
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,24 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {	// <?>: Unbounded WildCard, 어떤 자료형의 객체도 매개변수로 받겠다는 의미
+		return new Class[] { RootConfig.class };
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
