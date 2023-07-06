4f1f0100a6bd170d96d6c342a558e41e25f39751
ChoiHyeongBin <66659982+ChoiHyeongBin@users.noreply.github.com>
2023-07-06 16:45:33 +0900

    LV2 의상

--- ProgrammersCodingTest.java
+++ ProgrammersCodingTest.java
@@ -1,70 +1,53 @@
 package codingTest;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class ProgrammersCodingTest {
 
     public static void main(String[] args) {
-//    	int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
-    	int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
-//    	int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
-    	int[][] arr2 = {{3, 3}, {3, 3}};
+    	String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
     	
-    	solution(arr1, arr2);
+    	solution(clothes);
     }
 	
-    // LV2 행렬의 곱셈
-    public static int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
-//        System.out.println(Arrays.deepToString(answer));
+    // LV2 의상
+    public static int solution(String[][] clothes) {
+        int answer = 1;
         
-        for (int i = 0; i < arr1.length; ++i) {
-        	System.out.println(Arrays.toString(arr1[i]));
+        // 1. 옷을 종류별로 구분하기
+        HashMap<String, Integer> tempMap = new HashMap<>();
+        
+        for (String[] clothe : clothes) {
+        	String type = clothe[1];
+        	System.out.println("type: " + type);
         	
-        	for (int j = 0; j < arr2[0].length; ++j) {
-        		for (int k = 0; k < arr1[0].length; ++k) {
-        			System.out.println(arr1[i][k]);
-        			System.out.println(arr2[k][j]);
-        			System.out.println();
-        			
-        			answer[i][j] += arr1[i][k] * arr2[k][j];
-        		}
-        	}
+        	// getOrDefault: 찾는 키가 존재한다면 찾는 키의 값을 반환하고, 없다면 기본 값을 반환하는 메서드
+        	tempMap.put(type, tempMap.getOrDefault(type, 0) + 1);
+        	System.out.println("tempMap: " + tempMap);
         }
         
-        /* 실패
-        int cnt = 0;	// 배열 전체 개수
+        System.out.println();
+        System.out.println("tempMap 222: " + tempMap);
         
-        for (int i = 0; i < arr1.length; i++) {
-        	System.out.println(Arrays.toString(arr1[i]));
-
-        	cnt += arr1[i].length;
+        // 2. 입지 않는 경우를 추가하여 모든 조합 계산하기
+        // Iterator: 인덱스가 없는 자료구조의 경우 그 안의 데이터를 돌기 위해서 사용
+        Iterator<Integer> it = tempMap.values().iterator();
+        
+        while (it.hasNext()) {
+//        	System.out.println(it.next().intValue());
+        	
+        	answer *= it.next().intValue() + 1;
         }
         
-        System.out.println("cnt: " + cnt);
+        /* for (int i = 0; i < clothes.length; i++) {
+        	System.out.println(clothes[i][0]);
+        } */
         
-        for (int i = 0; i < arr1.length; i++) {
-        	System.out.println(Arrays.toString(arr1[i]));
-        	
-        	for (int j = 0; j < cnt; j++) {
-        		int n = 0;
-        		
-        		for (int k = 0; k < arr1[i].length; k++) {
-            		System.out.println(arr1[i][n] * arr2[k][i]);
-            		n++;
-            		
-            		if (n >= arr1[i].length) {
-            			n = 0;
-            		}
-                }
-        	}
-        	
-        	System.out.println();
-        }
-         */
-        
-        System.out.println(Arrays.deepToString(answer));
-        return answer;
+        System.out.println("answer: " + (answer - 1));
+        // 3. 아무종류의 옷도 입지 않는 경우 제외하기
+        return answer - 1;
     }
 	
 }
